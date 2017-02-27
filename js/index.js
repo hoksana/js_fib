39aa82d6def2ee49e859666f01666afdfb2dd5fb
Oksana <oksana.halus@gmail.com>
2017-02-27 14:20:21 +0000

    comrnt

--- js/index.js
+++ js/index.js
@@ -2,9 +2,9 @@
 var namb = 1;
 var nambe = 1;
 var nambr;
-var num = prompt("Напишіть будь ласка число");
-document.write(namb+"<br>");
-document.write(nambe+"<br>");
+var num = prompt('Напишіть будь ласка число');
+document.write(namb+'<br>');
+document.write(nambe+'<br>');
 
 for(var fib = 3; fib <= num; fib++)
 {
@@ -12,6 +12,6 @@ for(var fib = 3; fib <= num; fib++)
     namb = nambe;
     nambe = nambr;
     
-    document.write(nambr+"<br>");
+    document.write(nambr+'<br>');
 }
 
