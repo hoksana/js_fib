b00967a50ed79c88baa42279605d660027e3e0e8
Oksana <oksana.halus@gmail.com>
2017-02-27 14:32:57 +0000

    comit

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-
+/*
 var namb = 1;
 var nambe = 1;
 var nambr;
@@ -14,4 +14,33 @@ for(var fib = 3; fib <= num; fib++)
     
     document.write(nambr+'<br>');
 }
+*/
+function getInput(text) {
+  var input = Number(prompt(text));
 
+  return input = input ? input : getInput('Bad input. Give me a number position');
+}
+
+function findFibonacciLoop(position) {
+  var numbers = [1, 1];
+
+  for (var n = 2; n < position; n++) {
+    numbers[n] = numbers[n - 1] + numbers[n - 2];
+  }
+
+  return numbers[position - 1];
+}
+
+function findFibonacciRecursion(position) {
+  var numbers = [1, 1],
+    n = position - 1;
+
+  if (numbers[n]) { return numbers[n]; }
+
+  numbers[n] = numbers[n - 1] + numbers[n - 2];
+
+  return numbers[n] ? numbers[n] : findFibonacciRecursion(n) + findFibonacciRecursion(n - 1);
+}
+
+var position = getInput('Please enter a number position');
+document.write('Loop: ' + findFibonacciLoop(position) + '<br>' + 'Recursion: ' + findFibonacciRecursion(position));
